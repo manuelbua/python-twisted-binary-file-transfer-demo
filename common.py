0e02f9382806f3d6c43d648deb0dc6ad88f97b65
Manuel Bua <manuel.bua@gmail.com>
2013-10-02 22:16:51 +0200

    Fix messed up code layout

--- common.py
+++ common.py
@@ -1,30 +1,39 @@
 import hashlib
 from datetime import datetime
 
+
 COMMANDS = {
-            'list': ('list', 'Displays a list of all the available files'),
-            'get': ('get <remote filename>', 'Downloads a file with a given filename (use "get all" to retrieve all files)'),
-            'put': ('put <local file path> <remote file name>', 'Uploads a file with a given filename'),
-            'help': ('help', 'Displays a list of all the available commands'),
-            'quit': ('quit', 'Disconnects from the server'),
+    'list': ('list', 'Displays a list of all the available files'),
+    'get': ('get <remote filename>',
+            'Downloads a file with a given filename (use "get all" to '
+            'retrieve all files)'),
+    'put': ('put <local file path> <remote file name>',
+            'Uploads a file with a given filename'),
+    'help': ('help', 'Displays a list of all the available commands'),
+    'quit': ('quit', 'Disconnects from the server'),
 }
+
 
 def timestamp():
     """ Returns current time stamp. """
-    return '[%s]'  % (datetime.strftime(datetime.now(), '%H:%M:%S'))
+    return '[%s]' % (datetime.strftime(datetime.now(), '%H:%M:%S'))
+
 
 def display_message(message):
     """ Displays a message with a prepended time stamp. """
 
     print '%s %s' % (timestamp(), message)
 
+
 def validate_file_md5_hash(file, original_hash):
-    """ Returns true if file MD5 hash matches with the provided one, false otherwise. """
+    """ Returns true if file MD5 hash matches with the provided one,
+    false otherwise. """
 
     if get_file_md5_hash(file) == original_hash:
         return True
 
     return False
+
 
 def get_file_md5_hash(file):
     """ Returns file MD5 hash"""
@@ -35,7 +44,8 @@ def get_file_md5_hash(file):
 
     return md5_hash.hexdigest()
 
-def read_bytes_from_file(file, chunk_size = 8192):
+
+def read_bytes_from_file(file, chunk_size=8192):
     """ Read bytes from a file in chunks. """
 
     with open(file, 'rb') as file:
@@ -43,12 +53,14 @@ def read_bytes_from_file(file, chunk_size = 8192):
             chunk = file.read(chunk_size)
 
             if chunk:
-                    yield chunk
+                yield chunk
             else:
                 break
 
+
 def clean_and_split_input(input):
-    """ Removes carriage return and line feed characters and splits input on a single whitespace. """
+    """ Removes carriage return and line feed characters and splits input on
+    a single whitespace. """
 
     input = input.strip()
     input = input.split(' ')
