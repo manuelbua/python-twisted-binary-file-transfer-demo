aa1d9824c5633bac4726bf6a6a334f1393665c88
Manuel Bua <manuel.bua@gmail.com>
2013-10-02 12:39:21 +0200

    Add .gitignore, client percent completion, fixed bugs

--- common.py
+++ common.py
@@ -15,7 +15,7 @@ def timestamp():
 
 def display_message(message):
     """ Displays a message with a prepended time stamp. """
-    
+
     print '%s %s' % (timestamp(), message)
 
 def validate_file_md5_hash(file, original_hash):
@@ -23,25 +23,25 @@ def validate_file_md5_hash(file, original_hash):
 
     if get_file_md5_hash(file) == original_hash:
         return True
-        
+
     return False
 
 def get_file_md5_hash(file):
     """ Returns file MD5 hash"""
-    
+
     md5_hash = hashlib.md5()
     for bytes in read_bytes_from_file(file):
         md5_hash.update(bytes)
-        
+
     return md5_hash.hexdigest()
 
-def read_bytes_from_file(file, chunk_size = 8100):
+def read_bytes_from_file(file, chunk_size = 8192):
     """ Read bytes from a file in chunks. """
-    
+
     with open(file, 'rb') as file:
         while True:
             chunk = file.read(chunk_size)
-            
+
             if chunk:
                     yield chunk
             else:
@@ -49,8 +49,8 @@ def read_bytes_from_file(file, chunk_size = 8100):
 
 def clean_and_split_input(input):
     """ Removes carriage return and line feed characters and splits input on a single whitespace. """
-    
+
     input = input.strip()
     input = input.split(' ')
-        
+
     return input
